eff0ab701251b92b2941c7e09e1f65d87ec95a29
aman_cjinc <aman.cjinc@gmail.com>
2022-03-16 06:29:11 +0530

    initial setup

---
+++ gentleman/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Mockman from "mockman-js";
+function App() {
+  console.log(process.env);
+  return (
+    <div className="App">
+      <Mockman />
+    </div>
+  );
+}
+
+export default App;
