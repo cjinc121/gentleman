62ea210dbc03528f6c85042f47c79c8c0eaaa970
aman_cjinc <aman.cjinc@gmail.com>
2022-03-17 01:28:20 +0530

    minor refactoring

--- gentleman/src/App.js
+++ gentleman/src/App.js
@@ -4,10 +4,8 @@ import { Navbar } from "./components/navbar/Navbar.jsx";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  console.log(process.env);
   return (
     <div className="App">
-      {/* <Mockman /> */}
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/mockman" element={<Mockman />} />
