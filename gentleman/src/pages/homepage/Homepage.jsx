3d4b90610e9430c1cf312245a0504d83c97a418a
aman_cjinc <aman.cjinc@gmail.com>
2022-03-20 18:10:36 +0530

    code refactoring

--- gentleman/src/pages/homepage/Homepage.jsx
+++ gentleman/src/pages/homepage/Homepage.jsx
@@ -1,7 +1,27 @@
 import "./homepage.css";
+import axios from "axios";
 import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
 const Homepage=()=>{
-  return <div> <div className="banner-container">
+  const [category,setCategory]=useState([]);
+  const [featured,setFeatured]=useState([]);
+  //api call
+ useEffect(()=>{
+(async ()=>{
+  try{
+    const categoryResponse=await axios.get('/api/categories');
+    const productResponse=await axios.get('/api/products');
+    setCategory(categoryResponse.data.categories);
+    setFeatured( productResponse.data.products);
+  }
+  catch(err){
+    console.log(err);
+  }
+})()
+ },[])
+  return <div> 
+   {/* banner */}
+   <div className="banner-container">
   <div className="left-image">
     <img src="../../assets/7.png" alt="left-image" />
     <img src="../../assets/10.png" alt="right-image" />
@@ -21,92 +41,44 @@ const Homepage=()=>{
     <img src="../../assets/12.png" alt="right-image" />
   </div>
 </div>
+{/* category */}
 <div className="heading">Categories</div>
-<div className="category-container">
-  <div className="category-card">
-    <div className="top-image">
-      <Link to="/products">
-        <img src="../../assets/boots.jpg" alt="category-image" />
-      </Link>
-    </div>
-    <div className="bottom-name">BOOTS</div>
-  </div>
-  <div className="category-card">
-    <div className="top-image">
-    <Link to="/products">
-        <img src="../../assets/sports.jpg" alt="category-image" />
-      </Link>
-    </div>
-    <div className="bottom-name">SPORTS</div>
-  </div>
-  <div className="category-card bottom-column">
-    <div className="top-image">
-    <Link to="/products">
-        <img src="../../assets/office.jpg" alt="category-image" />
-      </Link>
-    </div>
-    <div className="bottom-name">OFFICE</div>
-  </div>
-  <div className="category-card bottom-column">
-    <div className="top-image">
-     <Link to="/products">
-        <img src="../../assets/sneaker.jpg" alt="category-image" />
-      </Link>
-    </div>
-    <div className="bottom-name">SNEAKER</div>
-  </div>
-  <div className="category-card bottom-column">
-    <div className="top-image">
-    <Link to="/products">
-        <img src="../../assets/sandals.jpg" alt="category-image" />
-      </Link>
-    </div>
-    <div className="bottom-name">SANDAL</div>
-  </div>
-</div>
-<div className="heading">Featured Products</div>
-<div className="featured-container">
-  <div className="card-container-vertical">
-    <img src="../../assets/left_image.jpg" />
-    <div className="card-vertical-title">
-      <h2 className="main-title">Card title</h2>
-      <p className="desc">description</p>
-      <p className="card-price">$1234 <s>$9999</s></p>
-      <p className="discount">(87% off)</p>
-      <Link to="/products"> <button className="button contained-button black-button">Buy Now</button></Link>
-    </div>
-  </div>
-  <div className="card-container-vertical">
-    <img src="../../assets/featured-1.jpg" />
-    <div className="card-vertical-title">
-      <h2 className="main-title">My Card Title</h2>
-      <p className="desc">description</p>
-      <p className="card-price">$1234 <s>$9999</s></p>
-      <p className="discount">(87% off)</p>
-      <Link to="/products"> <button className="button contained-button black-button">Buy Now</button></Link>
-    </div>
-  </div>
 
-  <div className="card-container-vertical">
-    <img src="../../assets/featured-2.jpg" />
-    <div className="card-vertical-title">
-      <h2 className="main-title">My Card Title</h2>
-      <p className="desc">description</p>
-      <p className="card-price">$1234 <s>$9999</s></p>
-      <p className="discount">(87% off)</p>
-      <Link to="/products"> <button className="button contained-button black-button">Buy Now</button></Link>
+<div className="category-container">
+{
+      category.map((item)=>{
+        return <div className="category-card">
+        <div className="top-image">
+          <Link to="/products">
+            <img src={item.photoUrl} alt="category-image" />
+          </Link>
+        </div>
+        <div className="bottom-name">{item.categoryName}</div>
+      </div>
+      })
+    }
+</div>
+{/* featured  */}
+<div className="heading">Featured Products</div>
+
+<div className="featured-container">
+  {
+    featured.filter((item)=>item.featuredProduct).map((item)=>{
+      return <div className="card-container-vertical">
+      <img src={item.photoUrl}/>
+      <div className="card-vertical-title">
+        <h2 className="main-title">{item.title}</h2>
+        <p className="desc">{item.categoryName}</p>
+        <p className="card-price">${item.discountPrice} <s>${item.originalPrice}</s></p>
+        <p className="discount">{(item.originalPrice-item.discountPrice)*100/item.originalPrice}%</p>
+        <Link to="/products"> <button className="button contained-button black-button">Buy Now</button></Link>
+      </div>
     </div>
-  </div>
-  <div className="card-container-vertical">
-    <img src="../../assets/boots.jpg" />
-    <div className="card-vertical-title">
-      <h2 className="main-title">My Card Title</h2>
-      <p className="desc">description</p>
-      <p className="card-price">$1234 <s>$9999</s></p>
-      <p className="discount">(87% off)</p>
-     <Link to="/products"> <button className="button contained-button black-button">Buy Now</button></Link>
-    </div>
-  </div>
-</div></div>
+    })
+     
+  }
+ 
+</div>
+</div>
 }
 export {Homepage};
