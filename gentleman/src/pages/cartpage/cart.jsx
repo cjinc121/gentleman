10062a21a8e616b3478b7a8c4fe8f548ccd4602c
aman_cjinc <aman.cjinc@gmail.com>
2022-04-25 05:30:49 +0530

    added auth and api calls

--- gentleman/src/pages/cartpage/cart.jsx
+++ gentleman/src/pages/cartpage/cart.jsx
@@ -1,86 +1,148 @@
 import { useUserContext } from "../../context/user-context";
 import "./cart.css";
-import {AiFillMinusCircle,AiFillPlusCircle} from "react-icons/ai";
-import {MdCancel} from "react-icons/md";
-import {BsFillCartXFill} from "react-icons/bs";
-const Cart=()=>{
-  const {userState,userDispatch }=useUserContext();
-  const totalQuantity=userState.cart.reduce((acc,curr)=>{
-    acc=acc+curr.quantity;
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
+import { BsFillCartXFill } from "react-icons/bs";
+const Cart = () => {
+  const {
+    userState,
+    addToWishlistHandler,
+    deleteFromCartHandler,
+    increaseQuantityCartHandler,
+    decreaseQuantityCartHandler,
+  } = useUserContext();
+  const totalQuantity = userState.cart.reduce((acc, curr) => {
+    acc = acc + curr.qty;
     return acc;
-  },0)
- const totalPrice=userState.cart.reduce((acc,curr)=>{
-   acc=acc+curr.product.discountPrice*curr.quantity;
-   return acc
- },0)
- const totalDiscount=userState.cart.reduce((acc,curr)=>{
-   acc=acc+(curr.product.originalPrice-curr.product.discountPrice)*curr.quantity;
-   return acc
- },0)
-  return <div> 
-    <div className="page-header">Your Cart({userState.cart.length})</div>
-    {userState.cart.length===0&&<div className="page-header">Empty<BsFillCartXFill/></div>}
-   {userState.cart.length!==0 &&<div><div className="cart-container">
-    <div className="cart-container-card">
-  {
-    userState.cart.map((item)=>{
-      let b="";
-      userState.wishlist.map((wishItem)=>{
-        if(wishItem.id===item.product.id)b="true";
-      });
-      return <div className="card-container-vertical">
-      <img className="card-image" src={item.product.photoUrl} />
-      <div className="card-vertical-title">
-        <h2 className="title main-title">{item.product.title}</h2>
-        <div className="tag-card-right cart-icon" onClick={()=>userDispatch({type:"REMOVE_FROM_CART",payload:item})}><MdCancel/></div>
-        <div className="cart-minus-plus">
-          <div className="button-container-item">
-            <button className="floating-button" onClick={()=>userDispatch({type:"INCREASE_QUANTITY",payload:item.product.id})} >
-            <AiFillPlusCircle className="cart-icon"/>   
-         </button>
-          </div>
-          <p>{item.quantity}</p>
-          <div className="button-container-item">
-            <button className="floating-button "onClick={()=>userDispatch({type:"DECREASE_QUANTITY",payload:item.product.id})}>
-            <AiFillMinusCircle className="cart-icon"/>
-            </button>
-          </div>
+  }, 0);
+  const totalPrice = userState.cart.reduce((acc, curr) => {
+    acc = acc + curr.discountPrice * curr.qty;
+    return acc;
+  }, 0);
+  const totalDiscount = userState.cart.reduce((acc, curr) => {
+    acc = acc + (curr.originalPrice - curr.discountPrice) * curr.qty;
+    return acc;
+  }, 0);
+  return (
+    <div>
+      <div className="page-header">Your Cart({userState.cart.length})</div>
+      {userState.cart.length === 0 && (
+        <div className="page-header">
+          Empty
+          <BsFillCartXFill />
         </div>
-        <p className="title card-price">
-         Rs. {item.product.discountPrice} <s>Rs.{item.product.originalPrice}</s> <span className="discount">({Math.round((item.product.originalPrice-item.product.discountPrice)*100/item.product.originalPrice)}% Off)</span>
-        </p>
-        
-   <button className="button outline-button secondary-button"onClick={()=>{userDispatch({type:"ADD_TO_WISHLIST",payload:item.product})
-  userDispatch({type:"REMOVE_FROM_CART",payload:item})}}>Move to Wishlist</button>
+      )}
+      {userState.cart.length !== 0 && (
+        <div>
+          <div className="cart-container">
+            <div className="cart-container-card">
+              {userState.cart.map((item) => {
+                let b = "";
+                userState.wishlist.map((wishItem) => {
+                  if (wishItem.id === item.id) b = "true";
+                });
+                return (
+                  <div className="card-container-vertical">
+                    <img className="card-image" src={item.photoUrl} />
+                    <div className="card-vertical-title">
+                      <h2 className="title main-title">{item.title}</h2>
+                      <div
+                        className="tag-card-right cart-icon"
+                        onClick={() => {
+                          deleteFromCartHandler(item._id);
+                        }}
+                      >
+                        <MdCancel />
+                      </div>
+                      <div className="cart-minus-plus">
+                        <div className="button-container-item">
+                          <button
+                            className="floating-button"
+                            onClick={() => {
+                              increaseQuantityCartHandler(item._id);
+                            }}
+                          >
+                            <AiFillPlusCircle className="cart-icon" />
+                          </button>
+                        </div>
+                        <p>{item.qty}</p>
+                        <div className="button-container-item">
+                          <button
+                            className="floating-button "
+                            onClick={() => {
+                              if (item.qty > 1)
+                                decreaseQuantityCartHandler(item._id);
+                            }}
+                          >
+                            <AiFillMinusCircle className="cart-icon" />
+                          </button>
+                        </div>
+                      </div>
+                      <p className="title card-price">
+                        Rs. {item.discountPrice} <s>Rs.{item.originalPrice}</s>{" "}
+                        <span className="discount">
+                          (
+                          {Math.round(
+                            ((item.originalPrice - item.discountPrice) * 100) /
+                              item.originalPrice
+                          )}
+                          % Off)
+                        </span>
+                      </p>
+                      {b ? (
+                        <button
+                          className="button outline-button secondary-button"
+                          onClick={() => {
+                            deleteFromCartHandler(item._id);
+                          }}
+                        >
+                          Move to Wishlist
+                        </button>
+                      ) : (
+                        <button
+                          className="button outline-button secondary-button"
+                          onClick={() => {
+                            addToWishlistHandler(item);
+                            deleteFromCartHandler(item._id);
+                          }}
+                        >
+                          Move to Wishlist
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
 
-      
-      </div>
-    </div>
-    })
-  }  
-     </div>
-
-     <div className="bill-cart">
-      <div className="bill-cart-card">
-        <div className="bill-cart-row bold-row">
-          <h3>PRICE DETAILS</h3>
+            <div className="bill-cart">
+              <div className="bill-cart-card">
+                <div className="bill-cart-row bold-row">
+                  <h3>PRICE DETAILS</h3>
+                </div>
+                <div className="bill-cart-row">
+                  Price ({totalQuantity} items){" "}
+                  <span>Rs.{totalPrice + totalDiscount}</span>
+                </div>
+                <div className="bill-cart-row">
+                  Discount <span>-Rs.{totalDiscount}</span>
+                </div>
+                <div className="bill-cart-row">
+                  Delivery Charges<span>Rs.0</span>
+                </div>
+                <div className="bill-cart-row bold-row border-top">
+                  TOTAL AMOUNT <span>Rs.{totalPrice}</span>
+                </div>
+                <p>You will save Rs.{totalDiscount} on this order</p>
+                <button className="button contained-button black-button">
+                  PLACE ORDER
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="bill-cart-row">Price ({totalQuantity} items) <span>Rs.{totalPrice+totalDiscount}</span></div>
-        <div className="bill-cart-row">Discount <span>-Rs.{totalDiscount}</span></div>
-        <div className="bill-cart-row">Delivery Charges<span>Rs.0</span></div>
-        <div className="bill-cart-row bold-row border-top">
-          TOTAL AMOUNT <span>Rs.{totalPrice}</span>
-        </div>
-        <p>You will save Rs.{totalDiscount} on this order</p>
-        <button className="button contained-button black-button">
-          PLACE ORDER
-        </button>
-      </div>
+      )}
     </div>
-   </div>
-    </div>
-    }
-  
-  </div>
-}
-export {Cart};
+  );
+};
+export { Cart };
